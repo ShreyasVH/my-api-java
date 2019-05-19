5834a6a5af07b5d9b2a4982966a09dcc2a051274
Shreyas <shreyas.hande@quikr.com>
2019-05-19 11:29:21 +0530

    adding cluster name for elastic client

--- app/myapi/services/elastic/ElasticService.java
+++ app/myapi/services/elastic/ElasticService.java
@@ -4,6 +4,7 @@ import myapi.constants.Constants;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import play.Logger;
 import play.Play;
 
 import java.net.InetSocketAddress;
@@ -13,15 +14,19 @@ import java.net.InetSocketAddress;
  */
 public class ElasticService extends ElasticSearch
 {
+    private final Logger.ALogger LOGGER = Logger.of(ElasticService.class);
+
     public ElasticService()
     {
-        client = new PreBuiltTransportClient(Settings.EMPTY);
+        Settings settings = Settings.builder().put("cluster.name", System.getenv("ELASTIC_CLUSTER")).build();
+        client = new PreBuiltTransportClient(settings);
         try
         {
             client.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress(System.getenv("ELASTIC_IP"), Integer.parseInt(System.getenv("ELASTIC_PORT")))));
-        } catch (Exception ex)
+        }
+        catch (Exception ex)
         {
-
+            LOGGER.error("Error while connecting to elastic client", ex);
         }
     }
 }
