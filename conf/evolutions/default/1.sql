99ae54b0caad388bd5346f2b39ac17163f93e35b
ShreyasVH <shreyas.hande@gmail.com>
2022-01-09 20:30:57 +0530

    cleaning the migrations file

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -55,39 +55,6 @@ CREATE TABLE `movie_director_map` (
   UNIQUE KEY `uk_mdm_move_director` (`movie_id`, `director_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `songs` (
-    `id` varchar(20) NOT NULL,
-    `name` varchar(100) NOT NULL,
-    `movie_id` int unsigned NOT NULL,
-    `size` int unsigned NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE `song_composer_map` (
- `id` varchar(20) NOT NULL,
- `song_id` varchar(20) NOT NULL,
- `composer_id` varchar(20) NOT NULL,
- PRIMARY KEY (`id`),
- UNIQUE KEY `uk_scm_song_composer` (`song_id`, `composer_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE `song_lyricist_map` (
- `id` varchar(20) NOT NULL,
- `song_id` varchar(20) NOT NULL,
- `lyricist_id` varchar(20) NOT NULL,
- PRIMARY KEY (`id`),
- UNIQUE KEY `uk_slm_song_lyricist` (`song_id`, `lyricist_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE `song_singer_map` (
-   `id` varchar(20) NOT NULL,
-   `song_id` varchar(20) NOT NULL,
-   `singer_id` varchar(20) NOT NULL,
-   PRIMARY KEY (`id`),
-   UNIQUE KEY `uk_scm_song_singer` (`song_id`, `singer_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 ALTER TABLE movies ADD CONSTRAINT fk_movies_language FOREIGN KEY (`language_id`) REFERENCES `languages_list` (`id`) on DELETE RESTRICT ON UPDATE RESTRICT;
 ALTER TABLE movies ADD CONSTRAINT fk_movies_format FOREIGN KEY (`format_id`) REFERENCES `formats_list` (`id`) on DELETE RESTRICT ON UPDATE RESTRICT;
 
@@ -96,12 +63,3 @@ ALTER TABLE movie_actor_map ADD CONSTRAINT fk_mam_actor FOREIGN KEY (`actor_id`)
 
 ALTER TABLE movie_director_map ADD CONSTRAINT fk_mdm_movie FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`) on DELETE RESTRICT ON UPDATE RESTRICT;
 ALTER TABLE movie_director_map ADD CONSTRAINT fk_mdm_director FOREIGN KEY (`director_id`) REFERENCES `artists_list` (`id`) on DELETE RESTRICT ON UPDATE RESTRICT;
-
-ALTER TABLE song_composer_map ADD CONSTRAINT fk_scm_song FOREIGN KEY (`song_id`) REFERENCES `songs` (`id`) on DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE song_composer_map ADD CONSTRAINT fk_scm_composer FOREIGN KEY (`composer_id`) REFERENCES `artists_list` (`id`) on DELETE RESTRICT ON UPDATE RESTRICT;
-
-ALTER TABLE song_lyricist_map ADD CONSTRAINT fk_slm_song FOREIGN KEY (`song_id`) REFERENCES `songs` (`id`) on DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE song_lyricist_map ADD CONSTRAINT fk_slm_lyricist FOREIGN KEY (`lyricist_id`) REFERENCES `artists_list` (`id`) on DELETE RESTRICT ON UPDATE RESTRICT;
-
-ALTER TABLE song_singer_map ADD CONSTRAINT fk_ssm_song FOREIGN KEY (`song_id`) REFERENCES `songs` (`id`) on DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE song_singer_map ADD CONSTRAINT fk_ssm_singer FOREIGN KEY (`singer_id`) REFERENCES `artists_list` (`id`) on DELETE RESTRICT ON UPDATE RESTRICT;
