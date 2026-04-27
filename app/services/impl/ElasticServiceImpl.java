8495f41fbad2ca9539031695a66200e672df974f
ShreyasVH <shreyas.deve.1@gmail.com>
2026-04-27 18:54:53 +0530

    trying timeout

--- app/services/impl/ElasticServiceImpl.java
+++ app/services/impl/ElasticServiceImpl.java
@@ -4,9 +4,14 @@ import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch._types.Result;
 import co.elastic.clients.elasticsearch.core.*;
 import co.elastic.clients.elasticsearch.core.search.Hit;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.rest5_client.Rest5ClientTransport;
+import co.elastic.clients.transport.rest5_client.low_level.Rest5Client;
 import com.google.inject.Inject;
 import enums.ErrorCode;
 import exceptions.InternalServerError;
+import org.apache.hc.core5.http.HttpHost;
+import org.apache.hc.core5.util.Timeout;
 import play.libs.Json;
 import responses.ElasticResponse;
 import responses.FilterResponse;
@@ -14,6 +19,7 @@ import services.ElasticService;
 import utils.Utils;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -24,18 +30,45 @@ public class ElasticServiceImpl implements ElasticService
     private final ElasticsearchClient client;
 
     @Inject
-    public ElasticServiceImpl() {
+    public ElasticServiceImpl() throws URISyntaxException {
         client = this.getClient();
     }
 
-    private ElasticsearchClient getClient() {
+//    private ElasticsearchClient getClient() {
+//        String serverUrl = "https://" + System.getenv("ELASTIC_IP_HTTP") + ":" + System.getenv("ELASTIC_PORT_HTTP");
+//        String apiKey = System.getenv("ELASTIC_API_KEY");
+//
+//        return ElasticsearchClient.of(b -> b
+//                .host(serverUrl)
+//                .apiKey(apiKey)
+//        );
+//    }
+
+    private ElasticsearchClient getClient() throws URISyntaxException {
         String serverUrl = "https://" + System.getenv("ELASTIC_IP_HTTP") + ":" + System.getenv("ELASTIC_PORT_HTTP");
         String apiKey = System.getenv("ELASTIC_API_KEY");
 
-        return ElasticsearchClient.of(b -> b
-                .host(serverUrl)
-                .apiKey(apiKey)
+        Rest5Client restClient = Rest5Client
+                .builder(HttpHost.create(serverUrl))
+                .setDefaultHeaders(new org.apache.hc.core5.http.Header[] {
+                        new org.apache.hc.core5.http.message.BasicHeader("Authorization", "ApiKey " + apiKey)
+                })
+                .setRequestConfigCallback(requestConfigBuilder -> requestConfigBuilder
+                        .setResponseTimeout(Timeout.ofSeconds(60))
+                        .setConnectionRequestTimeout(Timeout.ofSeconds(10))
+                )
+                .setConnectionConfigCallback(connectionConfigBuilder -> connectionConfigBuilder
+                        .setConnectTimeout(Timeout.ofSeconds(10))
+                        .setSocketTimeout(Timeout.ofSeconds(60))
+                )
+                .build();
+
+        Rest5ClientTransport transport = new Rest5ClientTransport(
+                restClient,
+                new JacksonJsonpMapper()
         );
+
+        return new ElasticsearchClient(transport);
     }
 
     public <T> FilterResponse<T> search(SearchRequest request, Class<T> documentClass)
