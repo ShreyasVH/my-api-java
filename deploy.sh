ff650f6bfbbc715aa428ea03a27974997a205f1f
ShreyasVH <shreyas.deve.1@gmail.com>
2026-05-14 19:23:48 +0530

    added logs

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 if ! lsof -i :$PORT > /dev/null; then
     echo "Starting"
-	./dist/bin/myapi -jvm-debug $DEBUG_PORT -Dhttp.port=$PORT > myapi.log 2>&1 &
+	./dist/bin/myapi -jvm-debug $DEBUG_PORT -Dhttp.port=$PORT > myapi.log 2>&1
+	echo "$JAVA_HOME" >> server.log
+    echo "$PATH" >> server.log
+    java --version >> server.log
 fi
