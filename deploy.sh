2566357f8aee1b84b44e43e1926c43ed319bdf0a
ShreyasVH <shreyas.deve.1@gmail.com>
2026-05-15 07:49:43 +0530

    removed logs

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
 if ! lsof -i :$PORT > /dev/null; then
     echo "Starting"
-	./dist/bin/myapi -jvm-debug $DEBUG_PORT -Dhttp.port=$PORT > myapi.log 2>&1
-	echo "$JAVA_HOME" >> server.log
-    echo "$PATH" >> server.log
-    java --version >> server.log
+	./dist/bin/myapi -jvm-debug $DEBUG_PORT -Dhttp.port=$PORT > myapi.log 2>&1 &
 fi
